4b68e82a6ac61a1e0b13e518b9d11400a568dfc7
Christian Aistleitner <christian@quelltextlich.at>
2022-11-18 14:12:13 +0100

    Provide start/stop tooling for easier ad-hoc coding

--- toy_motor_controller/__init__.py
+++ toy_motor_controller/__init__.py
@@ -2,4 +2,30 @@
 # GNU Affero General Public License v3.0 only (See LICENSE.txt)
 # SPDX-License-Identifier: AGPL-3.0-only
 
+import threading
+
+import dbus.mainloop.glib
+from gi.repository import GLib
+
+MAINLOOP = None
+
+
+def start():
+    global MAINLOOP
+
+    if MAINLOOP is None:
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+        MAINLOOP = GLib.MainLoop()
+
+        threading.Thread(target=MAINLOOP.run, args=()).start()
+
+    return MAINLOOP
+
+
+def stop():
+    if MAINLOOP is not None:
+        MAINLOOP.quit()
+
+
 __version__ = '0.0.1-alpha.0'

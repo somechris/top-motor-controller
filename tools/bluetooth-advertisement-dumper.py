ddfc383239510b2fefebde0abcc1f805c2c0c7c3
Christian Aistleitner <christian@quelltextlich.at>
2022-11-21 14:06:03 +0100

    tools/bluetooth-advertisement-dumper: Allow to dump device details

--- tools/bluetooth-advertisement-dumper.py
+++ tools/bluetooth-advertisement-dumper.py
@@ -6,8 +6,11 @@
 import argparse
 import logging
 
+from bluepy import btle
+
 import toy_motor_controller
 from toy_motor_controller.bus.bluez import get_scanner
+from toy_motor_controller.util import bytes_to_hex_string
 
 LOG_FORMAT = ('%(asctime)s.%(msecs)03d %(levelname)-5s [%(threadName)s] '
               '%(filename)s:%(lineno)d - %(message)s')
@@ -16,14 +19,92 @@ logging.basicConfig(format=LOG_FORMAT, datefmt=LOG_DATE_FORMAT)
 logger = logging.getLogger(__name__)
 
 
-def get_dumper(rawData, min_rssi, address):
+def dump_uuid(uuid, args):
+    ret = ''
+    if args.mnemonics != 'replace':
+        ret += f'uuid={uuid}'
+    if args.mnemonics != 'hide':
+        name = uuid.getCommonName()
+        uuid_str = str(uuid)
+        if name != uuid_str or args.mnemonics == 'replace':
+            if ret:
+                ret += ', '
+            ret += f'name={name}'
+    return ret
+
+
+def dump_descriptor(descriptor, args):
+    print('      Descriptor('
+          f'handle={descriptor.handle}, '
+          f'{dump_uuid(descriptor.uuid, args)})')
+
+
+def dump_characteristic(characteristic, args):
+    properties = [name
+                  for (mask, name) in btle.Characteristic.propNames.items()
+                  if ((characteristic.properties & mask) == mask)
+                  ]
+    print('    Characteristic('
+          f'handle={characteristic.handle}, '
+          f'val_handle={characteristic.valHandle}, '
+          f'{dump_uuid(characteristic.uuid, args)}, '
+          f'properties={properties})')
+    if (args.dump_characteristic_readable_data or args.dump_all) \
+            and characteristic.supportsRead():
+        data = characteristic.read()
+        try:
+            print(f'        data (utf8): {data.decode()}')
+        except UnicodeDecodeError:
+            # data cannot get decoded to utf8. So we skip it.
+            pass
+        print(f'        data  (hex): {bytes_to_hex_string(data, " ")}')
+        print(f'        data  (raw): {data}')
+        print(f'        data  (len): {len(data)}')
+
+    if args.dump_descriptors or args.dump_all:
+        for descriptor in characteristic.getDescriptors():
+            dump_descriptor(descriptor, args)
+
+
+def dump_service(service, args):
+    print(f'  Service({dump_uuid(service.uuid, args)}, '
+          f'handle_start={service.hndStart}, '
+          f'handle_end={service.hndEnd})')
+    if args.dump_characteristics or args.dump_all:
+        for characteristic in service.getCharacteristics():
+            dump_characteristic(characteristic, args)
+
+
+def dump_services(address, args):
+    device = btle.Peripheral(address)
+    for service in device.getServices():
+        dump_service(service, args)
+    device.disconnect()
+    print('---')
+
+
+def get_dumper(args):
+    address = args.address
+    min_rssi = args.min_rssi
     if address is not None:
         address = address.lower()
 
     def dumper(advertisement):
         if min_rssi is None or min_rssi <= advertisement.rssi:
             if address is None or address == advertisement.address.lower():
-                print(advertisement.__str__(rawData=rawData))
+                if args.mnemonics == 'hide':
+                    str_arg = 'replace'
+                elif args.mnemonics == 'add':
+                    str_arg = 'add'
+                elif args.mnemonics == 'replace':
+                    str_arg = None
+                else:
+                    raise RuntimeError('Logic error')
+                print(advertisement.__str__(raw_data=str_arg))
+
+                if args.dump_services or args.dump_all:
+                    dump_services(advertisement.address, args)
+
     return dumper
 
 
@@ -33,8 +114,7 @@ def main(args):
 
     logger.debug('Initializing scanner')
     scanner = get_scanner(active=args.active)
-    dumper = get_dumper(
-        rawData=args.raw_data, min_rssi=args.min_rssi, address=args.address)
+    dumper = get_dumper(args)
 
     logger.debug('Starting scanner')
     scanner.register(dumper)
@@ -62,9 +142,14 @@ def parse_arguments():
                         action='store_true',
                         help='dump only found advertisements')
 
-    parser.add_argument('--raw-data',
-                        action='store_true',
-                        help='show numeric advertisement keys')
+    parser.add_argument(
+        '--mnemonics',
+        choices=['replace', 'add', 'hide'],
+        default='replace',
+        help='controls how to show mnemonics for numeric values. "replace" '
+        'replaces numbers by their mnemonics if possible. "add" shows both '
+        'numbers and mnemonics. "hide" hides mnemonics and shows only '
+        'numbers')
 
     parser.add_argument('--min-rssi',
                         type=int, default=None,
@@ -79,6 +164,37 @@ def parse_arguments():
                         default=None,
                         help='only show matches from this address')
 
+    parser.add_argument(
+        '--dump-services',
+        action='store_true',
+        help='try to dump service data. This will get the scanner stuck, if '
+        'a device does not support service data extraction. Use "--address" '
+        'to limit to a single device, to avoid getting stuck when another '
+        'device\'s advertisement gets scanned before the target device.')
+
+    parser.add_argument(
+        '--dump-characteristics',
+        action='store_true',
+        help='Dump characteristi cs for each dumped service. This implies '
+        '--dump-services.')
+
+    parser.add_argument(
+        '--dump-descriptors',
+        action='store_true',
+        help='Dump descriptors for each dumped characteristic. This implies '
+        '--dump-characteristics')
+
+    parser.add_argument(
+        '--dump-characteristic-readable-data',
+        action='store_true',
+        help='Dumps the data that can be read from dumped characteristics. '
+        'This implies --dump-characteristics')
+
+    parser.add_argument(
+        '--dump-all',
+        action='store_true',
+        help='Turns all --dump-* arguments on')
+
     return parser.parse_args()
 
 
@@ -91,5 +207,14 @@ if __name__ == '__main__':
         logging.getLogger().setLevel(logging.INFO)
     elif args.verbose >= 2:
         logging.getLogger().setLevel(logging.DEBUG)
+
+    if args.dump_characteristic_readable_data:
+        args.dump_characteristics = True
+
+    if args.dump_descriptors:
+        args.dump_characteristics = True
+
+    if args.dump_characteristics:
+        args.dump_services = True
 
     main(args)
